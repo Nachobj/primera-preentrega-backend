2f24cac792eff192ba9ead29de52381af554219d
Ignacio Fernandez <nacho@MacBook-Air-de-Ignacio.local>
2024-03-28 23:46:35 -0300

    cambios desafio websockets

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const PUERTO = 8080;
 const productsRouter = require("./routes/products.router.js")
 const cartsRouter = require("./routes/carts.router.js")
+const exphbs = require("express-handlebars");
+const viewsRouter = require("./routes/views.router.js");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -11,8 +13,13 @@ app.get("/", (req, res) => {
     res.send("Funcionando");
 })
 
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
 app.use("/api", productsRouter);
 app.use("/api", cartsRouter);
+app.use("/", viewsRouter);
 
 app.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto http://localhost:${PUERTO}`);
